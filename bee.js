2f3bb905a7bc32583ffe47066b9e0f5164b30c6d
Peter Hagelund <peterhagelund@mac.com>
2021-08-16 06:25:57 -0400

    prettified

--- bee.js
+++ bee.js
@@ -1,40 +1,40 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 function readDictionary(path, middle, others) {
-    const all = middle + others;
-    return readFileSync(path, 'utf-8')
-        .split('\n')
-        .filter((line) => line.length > 3)
-        .filter((line) => !line.includes('\''))
-        .filter((line) => line[0] == line[0].toLowerCase())
-        .map((line) => line.toUpperCase())
-        .filter((line) => line.includes(middle))
-        .filter((line) => {
-            for (let i = 0; i < line.length; i++) {
-                if (!all.includes(line[i])) {
-                    return false;
-                }
-            }
-            return true;
-        })
-        .sort((a, b) => b.length - a.length || a.localeCompare(b));
+  const all = middle + others;
+  return readFileSync(path, "utf-8")
+    .split("\n")
+    .filter((line) => line.length > 3)
+    .filter((line) => !line.includes("'"))
+    .filter((line) => line[0] == line[0].toLowerCase())
+    .map((line) => line.toUpperCase())
+    .filter((line) => line.includes(middle))
+    .filter((line) => {
+      for (let i = 0; i < line.length; i++) {
+        if (!all.includes(line[i])) {
+          return false;
+        }
+      }
+      return true;
+    })
+    .sort((a, b) => b.length - a.length || a.localeCompare(b));
 }
 
 function bee(argv) {
-    const middle = argv.middle.toUpperCase();
-    if (middle.length != 1) {
-        console.log(`Expected a single middle letter but got ${middle.length}`);
-        return;
-    }
-    const others = argv.others.toUpperCase();
-    const size = new Set(argv.others).size;
-    if (size != 6) {
-        console.log(`Expected six unique other letters but got ${size}`);
-        return;
-    }
-    const words = readDictionary('dictionary.txt', middle, others);
-    console.log(`Read ${words.length} words.`);
-    words.forEach((word) => console.log(word));
+  const middle = argv.middle.toUpperCase();
+  if (middle.length != 1) {
+    console.log(`Expected a single middle letter but got ${middle.length}`);
+    return;
+  }
+  const others = argv.others.toUpperCase();
+  const size = new Set(argv.others).size;
+  if (size != 6) {
+    console.log(`Expected six unique other letters but got ${size}`);
+    return;
+  }
+  const words = readDictionary("dictionary.txt", middle, others);
+  console.log(`Read ${words.length} words.`);
+  words.forEach((word) => console.log(word));
 }
 
-export { bee };
+export default bee;
--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-import Yargs from 'yargs';
-import { bee } from './bee.js';
-import { box } from './box.js';
-
+import Yargs from "yargs";
+import bee from "./bee.js";
+import box from "./box.js";
 
 function main(argv) {
-    if (argv._.includes('bee')) {
-        bee(argv);
-    } else if (argv._.includes('box')) {
-        box(argv);
-    } else {
-        console.log('No command specified');
-    }
+  if (argv._.includes("bee")) {
+    bee(argv);
+  } else if (argv._.includes("box")) {
+    box(argv);
+  } else {
+    console.log("No command specified");
+  }
 }
 
 const argv = Yargs(process.argv.slice(2))
-    .command('box', 'Solve a letter boxed puzzle', {
-        sides: {alias: 's', describe: 'Specify the sides of the box', type: 'string', demandOption: true},
-        dictionary: {alias: 'd', describe: 'The dictionary to load', type: 'string', default: 'dictionary.txt'},
-        goal: {alias: 'g', describe: 'goal', type: 'number', default: 5},
-        maxDuration: {alias: 'm', describe: 'The maximum duration in seconds', type: 'number', default: 10}
-    })
-    .command('bee', 'Solve a spelling bee puzzle', {
-        middle: {alias: 'm', describe: 'The required, middle letter', type: 'string', demandOption: true},
-        others: {alias: 'o', describe: 'The other six letters', type: 'string', demandOption: true},
-        dictionary: {alias: 'd', describe: 'The dictionary to load', type: 'string', default: 'dictionary.txt'},
-    })
-    .option('h', {alias: 'help'})
-    .option('v', {alias: 'version'})
-    .argv;
+  .command("box", "Solve a letter boxed puzzle", {
+    sides: {
+      alias: "s",
+      describe: "Specify the sides of the box",
+      type: "string",
+      demandOption: true,
+    },
+    dictionary: {
+      alias: "d",
+      describe: "The dictionary to load",
+      type: "string",
+      default: "dictionary.txt",
+    },
+    goal: { alias: "g", describe: "goal", type: "number", default: 5 },
+    maxDuration: {
+      alias: "m",
+      describe: "The maximum duration in seconds",
+      type: "number",
+      default: 10,
+    },
+  })
+  .command("bee", "Solve a spelling bee puzzle", {
+    middle: {
+      alias: "m",
+      describe: "The required, middle letter",
+      type: "string",
+      demandOption: true,
+    },
+    others: {
+      alias: "o",
+      describe: "The other six letters",
+      type: "string",
+      demandOption: true,
+    },
+    dictionary: {
+      alias: "d",
+      describe: "The dictionary to load",
+      type: "string",
+      default: "dictionary.txt",
+    },
+  })
+  .option("h", { alias: "help" })
+  .option("v", { alias: "version" }).argv;
 
 main(argv);
